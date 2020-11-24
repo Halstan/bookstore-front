a77df94c6e1e9dee26a07807b295bc45557655eb
Enzo Arauco <enzoarauco@gmail.com>
2020-11-23 20:36:44 -0500

    second refactor to reactive forms

--- src/app/components/autor/form-autor/form-autor.component.ts
+++ src/app/components/autor/form-autor/form-autor.component.ts
@@ -3,6 +3,7 @@ import { Autor } from '../../../model/autor';
 import { AutorService } from '../../../service/autor.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-autor',
@@ -13,17 +14,23 @@ export class FormAutorComponent implements OnInit {
   autor: Autor = new Autor();
   title = 'Registrar Autor';
   errors: string[] = [];
+  formAutor: FormGroup;
 
   constructor(private autorService: AutorService,
               private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder) {
+
+    this.crearFormulario();
+    this.cargarAutor();
+
+  }
 
   ngOnInit(): void {
-    this.cargarAutor();
   }
 
   registrarAutor(): void{
-    this.autorService.insertarAutor(this.autor).subscribe(
+    this.autorService.insertarAutor(this.formAutor.value).subscribe(
       res => {
         this.router.navigate(['/autores']);
         Swal.fire('Autor registrado', `Autor ${res.nombreAutor} registrado con éxito`, 'success');
@@ -38,15 +45,37 @@ export class FormAutorComponent implements OnInit {
     this.activatedRoute.params.subscribe(param => {
       const id = param['id'];
       if (id){
+        this.title = 'Editar autor';
         this.autorService.getAutor(id).subscribe(
-          autor => this.autor = autor
+          autor => {
+            this.autor = autor;
+            this.cargarData();
+          }
         );
       }
     });
   }
 
+  cargarData(): void{
+    this.formAutor.reset({
+      nombreAutor: this.autor.nombreAutor,
+      urlFoto: this.autor.urlFoto,
+      apellido: this.autor.apellido,
+      fechaNacimiento: this.autor.fechaNacimiento
+    });
+  }
+
+  crearFormulario(): void{
+    this.formAutor = this.fb.group({
+      nombreAutor: ['', [Validators.required, Validators.maxLength(40)]],
+      urlFoto: ['', [Validators.required, Validators.minLength(30), Validators.maxLength(160)]],
+      apellido: ['', [Validators.required, Validators.maxLength(40)]],
+      fechaNacimiento: ['', [Validators.required]]
+    });
+  }
+
   actualizarAutor(): void{
-    this.autorService.actualizarAutor(this.autor).subscribe(
+    this.autorService.actualizarAutor(this.formAutor.value).subscribe(
       autor => {
         this.router.navigate(['/autores']);
         Swal.fire('Autor actualizado', `${autor.nombreAutor} ${autor.apellido} ha sido actualizado con éxito`, 'success');
--- src/app/components/editorial/form-editorial/form-editorial.component.ts
+++ src/app/components/editorial/form-editorial/form-editorial.component.ts
@@ -3,6 +3,7 @@ import { Editorial } from '../../../model/editorial';
 import { EditorialService } from '../../../service/editorial.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-editorial',
@@ -11,20 +12,26 @@ import Swal from 'sweetalert2';
 export class FormEditorialComponent implements OnInit {
 
   editorial: Editorial = new Editorial();
-  title = 'Registrar/Editar editorial';
+  title = 'Registrar editorial';
+  formEditorial: FormGroup;
   cantidad: number;
   errors: string[] = [];
 
   constructor(private editorialService: EditorialService,
               private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private fb: FormBuilder) {
+
+    this.crearFormulario();
+    this.cargarEditorial();
+
+    }
 
   ngOnInit(): void {
-    this.cargarEditorial();
   }
 
   registrarEditorial(): void{
-    this.editorialService.insertarEdiorial(this.editorial).subscribe(
+    this.editorialService.insertarEdiorial(this.formEditorial.value).subscribe(
       res => {
         this.router.navigate(['/editoriales']);
         Swal.fire('Categoria registrada', `${res.nombreEditorial} registrada con éxito`, 'success');
@@ -35,14 +42,32 @@ export class FormEditorialComponent implements OnInit {
     );
   }
 
+  crearFormulario(): void{
+    this.formEditorial = this.fb.group({
+      nombreEditorial: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      fundador: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      fechaFundacion: ['', Validators.required],
+    });
+  }
+
+  cargarData(): void{
+    this.formEditorial.reset({
+      nombreEditorial: this.editorial.nombreEditorial,
+      fundador: this.editorial.fundador,
+      fechaFundacion: this.editorial.fechaFundacion
+    });
+  }
+
   cargarEditorial(): void{
     this.activatedRoute.params.subscribe(param => {
       const id = param['id'];
       if (id){
+        this.title = 'Editar editorial';
         this.editorialService.getEditorial(id).subscribe(
           editorial => {
             this.editorial = editorial['Editorial'],
             this.cantidad = editorial['CantidadLibros'];
+            this.cargarData();
           }
         );
       }
@@ -50,7 +75,9 @@ export class FormEditorialComponent implements OnInit {
   }
 
   actualizarEditorial(): void{
-    this.editorialService.actualizarEdiorial(this.editorial).subscribe(
+    const editorial = this.formEditorial.value as Editorial;
+    editorial.idEditorial = this.editorial.idEditorial;
+    this.editorialService.actualizarEdiorial(editorial).subscribe(
       res => {
         this.router.navigate(['/editoriales']);
         Swal.fire('Categoria actualizada', `${res.nombreEditorial} actualizada con éxito`, 'success');
